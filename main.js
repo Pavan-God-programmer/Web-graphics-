6b27d517d439255ab9aae030574030689136df3a
Pavan-God-programmer <p.thamaraikannan@share.epsb.ca>
2022-11-02 11:22:10 -0600

    bdfbzdbzsd

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-// JavaScript Code
+// Drawing bacses
+
+// Setup the canavs
+let cnv = document.getElementById("myCanvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 800;
+cnv.height = 600;
+
+// let's da some stuff
+// Draw rectabngle
+
+ctx.strokeStyle = "purple";
+ctx.strokeRect(50, 20, 150, 50);
+// Set outlien
+// css colors
+ctx.fillStyle = "rgb(0, 255, 0)";
+ctx.fillRect(225, 50, 50, 50);
